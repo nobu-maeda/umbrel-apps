f361fd2059f4c281f81aff88478477c046306449
Nobu Maeda <nobu.maeda@proton.me>
2022-12-28 03:09:48 -0500

    Fixes to make Bisq Daemon Umbrel App install work

--- bisq-daemon/exports.sh
+++ bisq-daemon/exports.sh
@@ -1,4 +1,12 @@
-export APP_BISQ_DAEMON_APP_PORT="6967"
+export APP_BISQ_DAEMON_IP="10.21.22.68"
+export APP_BISQ_DAEMON_NODE_IP="10.21.21.69"
+
+export APP_BISQ_DAEMON_PORT="6967"
 export APP_BISQ_DAEMON_GRPC_PORT="6968"
 export APP_BISQ_DAEMON_NODE_PORT="9999"
-export APP_BISQ_DAEMON_API_PASS="BeCareful"
+export APP_BISQ_DAEMON_API_PASS="becareful"
+
+rest_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rest/hostname"
+grpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-grpc/hostname"
+export APP_BISQ_DAEMON_NODE_HIDDEN_SERVICE="$(cat "${rest_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
+export APP_BISQ_DAEMON_GRPC_HIDDEN_SERVICE="$(cat "${grpc_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
